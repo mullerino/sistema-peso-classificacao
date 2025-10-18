86d5fd10b34afc3599e0d23cc2de5450275bbe45
mullerino <leandromllr9@gmail.com>
2025-10-18 12:43:44 -0300

    chore: adiciona arquivos iniciais do projeto

---
+++ main/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+void app_main(void)
+{
+}
