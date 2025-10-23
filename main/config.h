0658a2d770d8d42c655a032fc13682e92a648756
mullerino <leandromllr9@gmail.com>
2025-10-23 08:59:53 -0300

    feat: adiciona consts para as gpios do rgb e buzzer

--- main/config.h
+++ main/config.h
@@ -5,3 +5,8 @@
 #define I2C_MASTER_NUM I2C_NUM_0
 #define I2C_MASTER_FREQ_HZ 100000
 #define DS3231_ADDR 0x68
+
+#define GPIO_LED_R 04
+#define GPIO_LED_G 13
+#define GPIO_LED_B 14
+#define GPIO_BUZZER 27
