778144fe0ffe910ba3377ca6232b0c292c1d0e06
mullerino <leandromllr9@gmail.com>
2025-11-05 09:49:52 -0300

    feat: implementa fluxo completo que integra todos os módulos

--- main/main.c
+++ main/main.c
@@ -2,32 +2,30 @@
 #include <time.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "tasks/task_deteccao.h"
+#include "tasks/task_classificacao.h"
 #include "freertos/queue.h"
 #include "tasks/task_registro.h"
+#include "tasks/task_leitura_peso.h"
+#include "tasks/task_feedback.h"
 #include "esp_log.h"
+
+static const char *TAG = "MAIN";
+SemaphoreHandle_t sem_item_detectado;
 
 void app_main(void)
 {
+  ESP_LOGI(TAG, "Inicializando sistema de verificação e classificação de peso...");
+
+  queue_peso = xQueueCreate(5, sizeof(PesoMedido));
   queue_pedido_hora = xQueueCreate(5, sizeof(PedidoHora));
+  queue_feedback = xQueueCreate(5, sizeof(feedback_event_t));
 
-  xTaskCreate(task_registro, "TaskRegistro", 4096, NULL, 5, NULL);
+  sem_item_detectado = xSemaphoreCreateBinary();
 
-  while (1)
-  {
-    PedidoHora pedido = {
-        .id_mensagem = 1,
-        .respostaQueue = xQueueCreate(1, sizeof(RespostaHora))};
-
-    xQueueSend(queue_pedido_hora, &pedido, portMAX_DELAY);
-
-    RespostaHora resposta;
-    if (xQueueReceive(pedido.respostaQueue, &resposta, pdMS_TO_TICKS(2000)))
-    {
-      char buffer[64];
-      strftime(buffer, sizeof(buffer), "%d/%m/%Y %H:%M:%S", &resposta.hora);
-      ESP_LOGI("APP_MAIN", "Hora recebida: %s", buffer);
-    }
-
-    vTaskDelay(pdMS_TO_TICKS(5000));
-  }
+  xTaskCreate(task_deteccao, "TaskDeteccao", 4096, NULL, 5, NULL);
+  xTaskCreate(task_leitura_peso, "TaskLeituraPeso", 4096, NULL, 5, NULL);
+  xTaskCreate(task_registro, "TaskRegistro", 4096, NULL, 4, NULL);
+  xTaskCreate(task_feedback, "TaskFeedback", 4096, NULL, 3, NULL);
+  xTaskCreate(task_classificacao, "TaskClassificacao", 4096, NULL, 6, NULL);
 }
